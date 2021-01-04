96581c887602411aee74526cff47d6ba47e8d5c3
palmercd <cameron.palmer@nih.gov>
2021-01-04 09:28:05 -0500

    style: refactor post hook to look cleaner

--- hooks/post_gen_project.bash
+++ hooks/post_gen_project.bash
@@ -4,8 +4,10 @@
 rm -Rf finter
 {%- endif %}
 
+autoconf_archive_github="https://raw.githubusercontent.com/autoconf-archive/autoconf-archive/master/m4"
+
 function download_file {
-    wget "$1" -P m4 > /dev/null 2>&1
+    wget "$autoconf_archive_github/$1" -P "$2" > /dev/null 2>&1
     if [[ "$?" != "0" ]] ; then
 	echo "post_gen_project: failed to download \"$1\""
 	exit 1
@@ -13,21 +15,26 @@ function download_file {
 }
 
 {%- if cookiecutter.require_boost_headers == "yes" %}
-download_file "https://raw.githubusercontent.com/autoconf-archive/autoconf-archive/master/m4/ax_boost_base.m4"
+download_file "ax_boost_base.m4" "m4"
 {%- endif %}
 
 {%- if cookiecutter.require_boost_iostreams == "yes" %}
-download_file "https://raw.githubusercontent.com/autoconf-archive/autoconf-archive/master/m4/ax_boost_iostreams.m4"
+download_file "ax_boost_iostreams.m4" "m4"
 {%- endif %}
 
 {%- if cookiecutter.require_boost_filesystem  == "yes" %}
-download_file "https://raw.githubusercontent.com/autoconf-archive/autoconf-archive/master/m4/ax_boost_filesystem.m4"
+download_file "ax_boost_filesystem.m4" "m4"
 {%- endif %}
 
 {%- if cookiecutter.require_boost_program_options == "yes" %}
-download_file "https://raw.githubusercontent.com/autoconf-archive/autoconf-archive/master/m4/ax_boost_program_options.m4"
+download_file "ax_boost_program_options.m4" "m4"
 {%- else %}
 rm {{ cookiecutter.repo_name }}/cargs.*
+{%- endif %}
+
+{%- if cookiecutter.include_finter == "yes" %}
+download_file "ax_check_zlib.m4" "m4"
+cp m4/ax_check_zlib.m4 finter/m4
 {%- endif %}
 
 {%- if cookiecutter.git_tracking == "yes" %}
